9d0f1589af45d3b78008eebb8b8b76450dbfd940
Shadhin <01shadhin33@gmail.com>
2023-08-11 02:31:23 +0600

    modify UI & add all tax logic

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+
+var tax0 = 0;
+var tax5 = 0;
+var tax10 = 0;
+var tax15 = 0;
+var tax20 = 0;
+var tax25 = 0;
+
+
 function calculateTax() {
     const salary = parseFloat(document.getElementById("salary").value);
-    const allowance = parseFloat(document.getElementById("allowance").value);
+    var allowance = parseFloat(document.getElementById("allowance").value);
 
-    if (isNaN(salary) || isNaN(allowance)) {
-        alert("Please enter valid numbers for Salary and Allowance.");
+    if (isNaN(salary)) {
+        alert("Please enter valid numbers for Salary.");
         return;
+    }
+
+    if (isNaN(allowance)) {
+        allowance = 12*200;
     }
 
     const yearlySalary = salary * 12;
@@ -13,7 +26,7 @@ function calculateTax() {
     const totalReceived = yearlySalary + (bonus*2) + allowance;
 
     document.getElementById("calculatedSalary").value = yearlySalary;
-    document.getElementById("bonus").value = bonus;
+    document.getElementById("bonus").innerHTML = bonus;
     document.getElementById("calculatedBonus").value = calculatedBonus;
     document.getElementById("totalReceived").value = totalReceived;
 
@@ -24,11 +37,21 @@ function calculateTax() {
     //const basicSalary = yearlySalary + bonus + allowance - (yearlySalary / 2 + yearlySalary / 10 + 30000);
 
 
-    const houseRent = basicSalary / 2;
-    const medical = basicSalary / 10;
+    const houseRent = Math.ceil(basicSalary / 2);
+    const medical = Math.ceil(basicSalary / 10);
     const conveyance = 30000;
     const festivalBonus = calculatedBonus;
     const total = basicSalary + houseRent + medical + conveyance + festivalBonus;
+
+
+    const allowableExpenses = Math.min(total / 3, 450000);
+    const totalTaxableIncome = total - allowableExpenses;
+
+    calculateIncomeTax(totalTaxableIncome);
+   
+    const totalTax = Math.ceil(tax0 + tax5 + tax10 + tax15 + tax20 + tax25);
+    const taxDeductiblePerMonth = Math.floor(totalTax / 12);
+
 
     // Display the results in the new table
     document.getElementById("basicSalary").value = basicSalary;
@@ -37,4 +60,74 @@ function calculateTax() {
     document.getElementById("conveyance").value = conveyance;
     document.getElementById("festivalBonus").value = festivalBonus;
     document.getElementById("total").value = total;
+
+    document.getElementById("allowableExpenses").value = allowableExpenses;
+    document.getElementById("totalTaxableIncome").value = totalTaxableIncome;
+    document.getElementById("tax0").value = tax0.toFixed(2);
+    document.getElementById("tax5").value = tax5.toFixed(2);
+    document.getElementById("tax10").value = tax10.toFixed(2);
+    document.getElementById("tax15").value = tax15.toFixed(2);
+    document.getElementById("tax20").value = tax20.toFixed(2);
+    document.getElementById("tax25").value = tax25.toFixed(2);
+    document.getElementById("totalTax").value = totalTax.toFixed(2);
+    document.getElementById("taxDeductiblePerMonth").value = taxDeductiblePerMonth.toFixed(2);
 }
+
+
+function calculateIncomeTax(income) {
+
+    if (income <= 350000) {
+        tax0 = 0; // No tax on the first Tk 3.5 lakh
+    } 
+    
+    else if (income <= 450000) {
+        tax5 = (income - 350000) * 0.05; // 5% tax on the next Tk 1 lakh
+    } 
+    
+    else if (income <= 750000) {
+        tax5 = 5000 // 5% tax on the first Tk 1 lakh
+
+        tax10 = (income - 450000) * 0.10;
+    } 
+    
+    else if (income <= 1150000) {
+        tax5 = 5000 // 5% tax on the first Tk 1 lakh
+        tax10 = 30000 // 10% tax on the next Tk 3 lakh
+
+        tax15 = (income - 750000) * 0.15;
+    } 
+    
+    else if (income <= 1650000) {
+        tax5 = 5000 // 5% tax on the first Tk 1 lakh
+        tax10 = 30000 // 10% tax on the next Tk 3 lakh
+        tax15 = 60000 // 15% tax on the next Tk 4 lakh
+
+        tax20 = (income - 1150000) * 0.20;
+    } 
+    
+    else {
+        tax5 = 5000 // 5% tax on the first Tk 1 lakh
+        tax10 = 30000 // 10% tax on the next Tk 3 lakh
+        tax15 = 60000 // 15% tax on the next Tk 4 lakh
+        tax20 = 80000 // 20% tax on the next Tk 5 lakh
+
+        tax25 = tax5 + tax10 + tax15 + tax20 + (income - 1650000) * 0.25;
+    }
+
+}
+
+
+function resetValues() {
+    const inputFields = document.querySelectorAll('input[type="number"], input[type="text"], label');
+    const bonusLabel = document.getElementById("bonus");
+
+    inputFields.forEach(field => {
+        field.value = "";
+    });
+
+    bonusLabel.textContent = "0";
+
+    // Set cursor focus to the Salary input field
+    document.getElementById("salary").focus();
+}
+
